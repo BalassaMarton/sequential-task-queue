a3518d867b2cd618083b144186cba1660bdff1f1
Balassa Márton <balassa.marton@intralog.hu>
2016-09-30 11:33:01 +0200

    Improved gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,11 @@ var template = require("gulp-template");
 var rename = require("gulp-rename");
 var fs = require("fs");
 var del = require("del");
+var sequence = require("run-sequence");
 
-gulp.task("clean:dist", function() {
-    return del("dist/**/*");
-});
-
-gulp.task("clean:tsc", function() {
-    return del(["src/**/*.js", "test/**/*.js"]);
-});
-
-gulp.task("tsc", ["clean:tsc"], function(){
+gulp.task("tsc", function(){
     var proj = ts.createProject("tsconfig.json");
-    return gulp.src(["src/*.ts", "test/*.ts"])
+    return gulp.src(["./src/*.ts", "./test/*.ts", "./examples/*.ts"])
         .pipe(sourceMaps.init())
         .pipe(proj()).js
         .pipe(sourceMaps.write())
@@ -27,23 +20,12 @@ gulp.task("tsc", ["clean:tsc"], function(){
         }));
 });
 
-gulp.task("test-examples", function() {
-    var proj = ts.createProject("tsconfig.json");
-    return del("examples/**/*.js")
-        .then(() => {
-            return gulp.src(["examples/*.ts"])
-                .pipe(proj()).js
-                .pipe(gulp.dest("examples"))
-                .pipe(mocha({}));
-        }); 
-});
-
-gulp.task("doc:readme", ["test-examples"], function() {
-    var examples = fs.readFileSync("examples/examples.ts", "utf8");
+gulp.task("doc:readme", function() {
+    var examples = fs.readFileSync("./examples/examples.ts", "utf8");
     var data = {
         examples: snip(examples, { unindent: true })
     };
-    return gulp.src("doc/readme.template.md")
+    return gulp.src("./doc/readme.template.md")
         .pipe(template(data))
         .pipe(rename(function(path){
             path.basename = path.basename.replace(".template", "");
@@ -54,18 +36,29 @@ gulp.task("doc:readme", ["test-examples"], function() {
 
 gulp.task("doc", ["doc:readme"], function(){});
 
-gulp.task("test", ["tsc"], function() {
+gulp.task("test:examples", function() {
+    return gulp.src("./examples/*.js")
+        .pipe(mocha({})); 
+});
+
+gulp.task("test:lib", function() {
     return gulp.src("./test/*.js")
         .pipe(mocha({}));
 });
 
-gulp.task("build", ["clean:dist", "doc"], function() {
-    var proj = ts.createProject("tsconfig.json");
-    var result = gulp.src("src/*.ts")
+gulp.task("test", function(done){
+    // Running Mocha tests in parallel resulted in weird errors, must run in sequence. 
+    // Also: removed the "tsc" task from subtask dependencies, so running them individually may fail  
+    sequence("tsc", "test:lib", "test:examples", done);    
+});
+
+gulp.task("build", function() {
+    var proj = ts.createProject("./tsconfig.json");
+    var result = gulp.src("./src/*.ts")
         .pipe(sourceMaps.init())
         .pipe(proj());
-    result.dts.pipe(gulp.dest("dist/types"));
+    result.dts.pipe(gulp.dest("./dist/types"));
     result.js
         .pipe(sourceMaps.write())
-        .pipe(gulp.dest("dist/lib"));
+        .pipe(gulp.dest("./dist/lib"));
 });
